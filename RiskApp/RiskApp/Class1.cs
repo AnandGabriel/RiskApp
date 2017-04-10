f4d761dcce681d83d0cef8de91f158bb4d28e2ca
Anand Gabriel <anand_gabriel@yahoo.com>
2017-04-10 21:15:55 +1000

    Created the solution

---
+++ RiskApp/RiskApp/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskApp
+{
+    /// <summary>
+    /// Holds bet details
+    /// </summary>
+    public class Bet
+    {
+        private int CustomerID;
+        private int EventID;
+        private int ParticipantID;
+        private decimal Stake;
+        private decimal Win;
+
+        public Bet(int customerID, int eventID, int participantID, decimal stake, decimal win)
+        {
+            CustomerID = customerID;
+            EventID = eventID;
+            ParticipantID = participantID;
+            Stake = stake;
+            Win = win;
+        }
+    }
+}
