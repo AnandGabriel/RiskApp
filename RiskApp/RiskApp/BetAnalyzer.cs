e7d31018185ace2196d8647f78519694b2cbf883
Anand Gabriel <anand_gabriel@yahoo.com>
2017-04-10 21:55:47 +1000

    Added BetAnalyzer class

---
+++ RiskApp/RiskApp/BetAnalyzer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskApp
+{
+    class BetAnalyzer : IBetAnalyzer
+    {
+        private List<Bet> Bets;
+
+        public BetAnalyzer(List<Bet> bets)
+        {
+            Bets = bets;
+        }
+
+        public List<BetStatistics> AnalyzeBets()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ RiskApp/RiskApp/BetStatistics.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiskApp
+{
+    class BetStatistics
+    {
+        private int CustomerID;
+        private int WinPercentage;
+        private int AverageBet;
+        
+        public BetStatistics(int customerID, int winPercentage, int averageBet)
+        {
+            CustomerID = customerID;
+            WinPercentage = winPercentage;
+            AverageBet = averageBet;
+        }
+    }
+}
